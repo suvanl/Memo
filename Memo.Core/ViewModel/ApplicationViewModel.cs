74fcdcf4a8dd37ae91e1424441eb67fa8f9b7f45
Suvan <mr.slrmx@gmail.com>
2018-07-29 17:59:57 +0100

    Create and bind chat message input box control

--- Memo.Core/ViewModel/ApplicationViewModel.cs
+++ Memo.Core/ViewModel/ApplicationViewModel.cs
@@ -7,9 +7,9 @@ namespace Memo.Core
     /// </summary>
     public class ApplicationViewModel : BaseViewModel
     {
-        public ApplicationPage CurrentPage { get; private set; } = ApplicationPage.Login;
+        public ApplicationPage CurrentPage { get; private set; } = ApplicationPage.Chat;
 
-        public bool SideMenuVisible { get; set; } = false;
+        public bool SideMenuVisible { get; set; } = true;
 
         public void GoToPage(ApplicationPage page)
         {
